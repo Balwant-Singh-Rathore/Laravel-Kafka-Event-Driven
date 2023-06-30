e70cbad56c9facf00dc729dd3ad5955d53cee356
opensourcedept-simformsolutions <balwant@simformsolutions.com>
2023-06-30 16:55:38 +0530

    update scripts

---
+++ database/migrations/2023_06_30_111953_create_movies_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('movies', function (Blueprint $table) {
+            $table->id('movie_id');
+            $table->string('title')->nullable();
+            $table->integer('release_year')->nullable();
+            $table->string('country')->nullable();
+            $table->string('actors', 1024)->nullable();
+            $table->string('genres')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('movies');
+    }
+};
--- start.sh
+++ start.sh
@@ -13,9 +13,10 @@ echo -e "${BLUE}☁️ Confluent Platform is starting up...${NC}"
 export CONNECT_HOST=connect-debezium
 echo -e "\n--\n\n$(date) Waiting for Kafka Connect to start on ${GREEN}$CONNECT_HOST${NC}… ⏳"
 grep -q "Kafka Connect started" <(docker-compose logs -f $CONNECT_HOST)
+# Wait for the connect-debezium service to start
+sleep 15
 echo -e "\n--\n$(date) 👉 Creating Debezium connector"
 . ./scripts/submit_debezium_config.sh
 
 echo -e "\n--\n$(date) 👉 Validating the setup by displaying topics in Kafka cluster"
 docker exec -i kafka /usr/bin/kafka-topics --bootstrap-server kafka:9092 --list
-#docker exec -it kafka kafka-console-consumer --bootstrap-server kafka:9092  --topic movies --from-beginning
